f573af76fe0aeefcca1de28093655a1b9c8bb96e
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-03-01 15:21:19 +0300

    data generators are constrained to accept only torch.tensor-type input. dx, dy is taken at runtime to accept multidimensional input-output

--- data/data_generators.py
+++ data/data_generators.py
@@ -1,35 +1,38 @@
 import torch
 
-def generate_cx_sigm(x):
+def generate_cx_sigm(x:torch.Tensor):
     # rand_btw_7-30 * {-1, 1}
-    c1 = torch.rand(len(x)) * 1 + 7 #* torch.from_numpy(np.random.choice([-1, 1], (n, 1)))  # c1 for steepness
+    c1 = torch.rand_like(x) * 1 + 7 #* torch.from_numpy(np.random.choice([-1, 1], (n, 1)))  # c1 for steepness
     c2 = 0.5  # c2 midpoint
-    
-    data = 1/(1 + torch.exp(-c1 * (x-c2))) + torch.clamp(torch.randn(len(x))*1e-2**0.5, min=0)
+
+    noise = torch.clamp(torch.randn_like(x)*1e-4**0.5, min=0)
+    data = 1/(1 + torch.exp(-c1 * (x-c2))) + noise
     return data
 
 
-def generate_reverse_cx_sigm(x):
+def generate_reverse_cx_sigm(x:torch.Tensor):
     return 1-generate_cx_sigm(x)
 
 
-def generate_sin(x, amp=1, omega=1, eps=0.1):
-    noise = torch.clamp(torch.randn(len(x))*1e-2**0.5, min=0)
+def generate_sin(x:torch.Tensor):
+    dx = x.shape[-1]
+    noise = torch.clamp(torch.randn_like(x)*1e-4**0.5, min=0).view(-1, dx)
     return torch.sin(x*torch.pi) + noise
 
 
-def generate_cos(x, amp=1, omega=1, eps=0.1):
-    noise = torch.clamp(torch.randn(len(x))*1e-2**0.5, min=0)
+def generate_cos(x:torch.Tensor):
+    dx = x.shape[-1]
+    noise = torch.clamp(torch.randn_like(x)*1e-4**0.5, min=0).view(-1, dx)
     return torch.cos((0.5+x)*torch.pi) + 1 + noise
 
-def generate_chainsaw(x, amp=1, mod=0.5, eps=0.1):
+def generate_chainsaw(x:torch.Tensor, amp=1, mod=0.5, eps=0.1):
     r1 = torch.rand(1)*(eps*2)+(1-eps)
     r2 = torch.rand(1)*(eps*2)+(1-eps)
     r3 = torch.rand(1)*(eps*2)+(1-eps)
     return r1*amp*((x+r2) % (mod))+r3-amp/4
 
 
-def generate_reverse_chainsaw(x, amp=1, mod=0.5, eps=0.1):
+def generate_reverse_chainsaw(x:torch.Tensor, amp=1, mod=0.5, eps=0.1):
     r1 = torch.rand(1)*(eps*2)+(1-eps)
     r2 = torch.rand(1)*(eps*2)+(1-eps)
     r3 = torch.rand(1)*(eps*2)+(1-eps)
