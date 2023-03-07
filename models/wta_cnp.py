134fa65c74d43248068a3ebcb40a6bfa05760767
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-03-07 19:05:16 +0300

    incomplete for the moment

--- models/wta_cnp.py
+++ models/wta_cnp.py
@@ -44,21 +44,37 @@ class WTA_CNP(nn.Module):
     def forward(self, obs, tar):
         # obs: (batch_size, n_o (<n_max_obs), input_dim+output_dim)
         # tar: (batch_size, n_t (<n_max_tar), input_dim)
+        n_t = tar.shape[1]
         encoded_obs = self.encoder(obs)  # (batch_size, n_o (<n_max_obs), hidden_dim)
         encoded_rep = encoded_obs.mean(dim=1).unsqueeze(1)  # (batch_size, 1, hidden_dim)
-        repeated_encoded_rep = torch.repeat_interleave(encoded_rep, tar.shape[1], dim=1)  # each encoded_rep is repeated to match tar
+        repeated_encoded_rep = torch.repeat_interleave(encoded_rep, n_t, dim=1)  # each encoded_rep is repeated to match tar
         rep_tar = torch.cat([repeated_encoded_rep, tar], dim=-1)
         
-        pred = self.decoders(rep_tar)
+        pred = torch.zeros(self.num_decoders, self.batch_size, n_t, self.output_dim*2, device=tar.device)  # tar is used to get device
+
+        for i in range(self.num_decoders):
+            pred[i] = self.decoders[i](rep_tar)
+
+        gate_vals = self.gate(encoded_rep)  # (batch_size, num_decoders)
         
-        return pred
+        return pred, gate_vals
 
-    def loss(self, pred, real):
-        # pred: (batch_size, n_t (<n_max_tar), 2*output_dim)
+    def loss(self, pred, gate_vals, real):
+        # pred: (num_decoders, batch_size, n_t (<n_max_tar), 2*output_dim)
         # real: (batch_size, n_t (<n_max_tar), output_dim)
-        pred_mean = pred[:, :, :self.output_dim]
-        pred_std = nn.functional.softplus(pred[:, :, self.output_dim:])
 
-        pred_dist = torch.distributions.Normal(pred_mean, pred_std)
+        pred_means = pred[:, :, :, :self.output_dim]
+        pred_stds = nn.functional.softplus(pred[:, :, :, self.output_dim:])
 
-        return (-pred_dist.log_prob(real)).mean()
+        pred_dists = torch.distributions.Normal(pred_means, pred_stds)  # <num_decoders>-many gaussians
+        dec_loss = (-pred_dists.log_prob(real)).mean((-2, -1))  # (num_decoders, batch_size) - mean over tar and output_dim
+
+        #############
+
+        nll = torch.matmul(gate_vals, dec_loss).mean()  # (batch_size, batch_size)
+
+        #############
+
+        doubt = torch.prod(gate_vals, dim=1).mean()  # scalar
+
+        return nll + doubt
