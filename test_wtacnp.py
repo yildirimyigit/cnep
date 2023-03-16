11c3bbc31cedeffca1d6dd68dff8b9d73ebcb5d4
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-03-16 14:40:46 +0300

    ipynb 2 py for running remotely

--- test_wtacnp.py
+++ test_wtacnp.py
@@ -1,6 +1,6 @@
 # %%
 import torch
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 from models.wta_cnp import WTA_CNP
 from data.data_generators import *
@@ -47,12 +47,12 @@ x = torch.unsqueeze(x.repeat(num_classes, 1), 2)  # since dx = 1
 vx = torch.unsqueeze(vx.repeat(num_classes, 1), 2)
 print("X:", x.shape, "Y:", y.shape, "VX:", vx.shape, "VY:", vy.shape)
 
-#for i in range(num_indiv):
-#    plt.plot(x[i, :, 0], y[i, :, 0], 'r', alpha=0.3)
-#    plt.plot(x[i+num_indiv, :, 0], y[i+num_indiv, :, 0], 'g', alpha=0.3)
-#    plt.plot(x[i+2*num_indiv, :, 0], y[i+2*num_indiv, :, 0], 'b', alpha=0.3)
-#    plt.plot(x[i+3*num_indiv, :, 0], y[i+3*num_indiv, :, 0], 'magenta', alpha=0.3)
-#plt.show()
+for i in range(num_indiv):
+    plt.plot(x[i, :, 0], y[i, :, 0], 'r', alpha=0.3)
+    plt.plot(x[i+num_indiv, :, 0], y[i+num_indiv, :, 0], 'g', alpha=0.3)
+    plt.plot(x[i+2*num_indiv, :, 0], y[i+2*num_indiv, :, 0], 'b', alpha=0.3)
+    plt.plot(x[i+3*num_indiv, :, 0], y[i+3*num_indiv, :, 0], 'magenta', alpha=0.3)
+plt.show()
 
 x, y = x.to(device), y.to(device)
 
@@ -78,15 +78,31 @@ def get_batch(x, y, traj_ids):
     return obs, tar, tar_val
 
 # %%
-model = WTA_CNP(1, 1, 10, 10, [256, 256], 4, [128], 32).to(device)
+def get_validation_batch(vx, vy, o_ids=[0, -1]):
+    obs = torch.cat((vx[:, o_ids, :], vy[:, o_ids, :]), dim=-1).to(device)
+    tar = vx[:, torch.arange(t_steps)].to(device)
+    tar_val= vy[:, torch.arange(t_steps)].to(device)
+
+    return obs, tar, tar_val
+
+# %%
+model = WTA_CNP(1, 1, 10, 10, [256, 256, 256], 4, [128, 128], 32).to(device)
 optimizer = torch.optim.Adam(lr=1e-4, params=model.parameters())
 # model
 
 # %%
+import time
+
+file_name = int(time.time())
+
 epochs = 5000000
 epoch_iter = num_demos//batch_size  # number of batches per epoch (e.g. 100//32 = 3)
-min_loss = 1000000
 avg_loss = 0
+
+val_per_epoch = 1000
+min_val_loss = 1000000
+
+mse_loss = torch.nn.MSELoss()
 
 for epoch in range(epochs):
     epoch_loss = 0
@@ -103,9 +119,20 @@ for epoch in range(epochs):
 
         epoch_loss += loss.item()
 
-    if epoch_loss < min_loss and epoch > 5e4:
-        min_loss = epoch_loss
-        torch.save(model.state_dict(), f'_best_test_wtacnp_synth.pt')
+    if epoch % val_per_epoch == 0:
+        o, t, tr = get_validation_batch(vx, vy)
+
+        with torch.no_grad():
+            p, g = model(o, t)
+            # dec_id = torch.zeros_like(g, device=device)
+            dec_id = torch.argmax(g.squeeze(1), dim=-1)
+            vp_means = p[dec_id, torch.arange(batch_size), :, :dy]
+            val_loss = mse_loss(vp_means, tr).item()
+
+            if val_loss < min_val_loss:
+                min_val_loss = val_loss
+                print(f'New best: {min_val_loss}')
+                torch.save(model.state_dict(), f'saved_models/wtacnp_synth_{file_name}.pt')
 
     avg_loss += epoch_loss
 
@@ -113,32 +140,33 @@ for epoch in range(epochs):
         print("Epoch: {}, Loss: {}".format(epoch, avg_loss/100))
         avg_loss = 0
 
-    if epoch % 5000 == 0:
-        print(gate)
-
-# %%
-def get_validation_batch(o_ids=[0, -1]):
-    obs = torch.cat((vx[:, o_ids, :], vy[:, o_ids, :]), dim=-1)
-    tar = vx[:, torch.arange(t_steps)]
-    tar_val= vy[:, torch.arange(t_steps)]
-
-    return obs, tar, tar_val
 
 # %%
 # Testing the best model
-model = WTA_CNP(batch_size=batch_size)
-model.load_state_dict(torch.load('_best_test_wtacnp_synth.pt'))
-model.eval()
+# model = WTA_CNP(1, 1, 10, 10, [256, 256, 256], 4, [128, 128], batch_size)
+# model.load_state_dict(torch.load(f'saved_models/wtacnp_synth_{file_name}.pt'))
+# model.eval()
 
-o, t, tr = get_validation_batch()
+# o, t, tr = get_validation_batch(vx, vy)
 
-with torch.no_grad():
-    p, g = model(o, t)
+# with torch.no_grad():
+#     p, g = model(o, t)
 
-p, g = p.cpu().numpy(), g.cpu()
-t, tr = t.cpu().numpy(), tr.cpu().numpy()
+# p, g = p.cpu().numpy(), g.cpu()
+# t, tr = t.cpu().numpy(), tr.cpu().numpy()
 
-for i in range(batch_size):
-    dec_id = torch.argmax(g[i, :, :]).item()
-    print(dec_id)
-    # plt.plot(range(t_steps), p[dec_id, i, :, 0], colors[dec_id], alpha=0.3)
+# for i in range(batch_size):
+#     dec_id = torch.argmax(g[i, :, :]).item()
+#     print(dec_id)
+#     plt.plot(range(t_steps), p[dec_id, i, :, 0], colors[dec_id], alpha=0.3)
+
+# %%
+# pytorch_total_params = sum(p.numel() for p in model.parameters())
+# print(pytorch_total_params)
+# pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+# print(pytorch_total_params)
+
+# %%
+
+
+
