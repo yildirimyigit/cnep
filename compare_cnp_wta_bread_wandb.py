serious bug in wta loss calculation

compare_cnp_wta_bread_wandb.py
@@ -41,7 +41,8 @@ vx = torch.linspace(0, 1, 200).repeat(num_val_indiv, 1)
 vy = torch.zeros(num_val, t_steps, dy)
 
 # noise = torch.clamp(torch.randn(x.shape)*1e-7**0.5, min=0) - noise_clip
-coeff = (torch.rand(num_indiv)*0.75+0.25).unsqueeze(-1)
+# coeff = (torch.rand(num_indiv)*0.75+0.25).unsqueeze(-1)
+coeff = (torch.tensor([0.6])).unsqueeze(-1)
 y[:num_indiv] = torch.unsqueeze(generate_sin(x)*coeff, 2)
 y[num_indiv:] = -1 * y[:num_indiv]
 
@@ -140,10 +141,10 @@ sweep_config = {
 
 # sweep_id = wandb.sweep(
 #  sweep=sweep_config,
-#  project='bread-loss-components-sweep'
+#  project='bread-loss-components-sweep-2'
 #  )
 
-sweep_id = 'ngtolbuu'
+sweep_id = 'r7lk50k0'
 
 import time
 import os
@@ -155,7 +156,8 @@ def train():
     entropy_coef = wandb.config.entropy_coef
     gate_std_coef = wandb.config.gate_std_coef
 
-    model_wta = WTA_CNP(1, 1, n_max_obs, n_max_tar, [128, 128, 128], num_decoders=2, decoder_hidden_dims=[128, 128], batch_size=batch_size).to(device_wta)
+    model_wta = WTA_CNP(1, 1, n_max_obs, n_max_tar, [128, 128, 128], num_decoders=2, decoder_hidden_dims=[128, 128], batch_size=batch_size,
+                        nll_coef=nll_coef, entropy_coef=entropy_coef, gate_std_coef=gate_std_coef).to(device_wta)
     optimizer_wta = torch.optim.Adam(lr=1e-4, params=model_wta.parameters())
 
     model_cnp = CNP(input_dim=1, hidden_dim=128, output_dim=1, n_max_obs=n_max_obs, n_max_tar=n_max_tar, num_layers=3, batch_size=batch_size).to(device_cnp)
@@ -273,4 +275,4 @@ def train():
             torch.save(torch.Tensor(training_loss_cnp), cnp_tr_loss_path)
             torch.save(torch.Tensor(validation_error_cnp), cnp_val_err_path)
 
-wandb.agent(sweep_id, function=train, count=16, project='bread-loss-components-sweep')
+wandb.agent(sweep_id, function=train, count=16, project='bread-loss-components-sweep-2')

models/wta_cnp.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class WTA_CNP(nn.Module):
     def __init__(self, input_dim=1, output_dim=1, n_max_obs=10, n_max_tar=10, encoder_hidden_dims=[256,256,256],
-                 num_decoders=4, decoder_hidden_dims=[128,128], batch_size=32, nll_coef=3.0, entropy_coef=10.0, gate_std_coef=1.0):
+                 num_decoders=4, decoder_hidden_dims=[128,128], batch_size=32, nll_coef=1, entropy_coef=0.28, gate_std_coef=0.274):
         super(WTA_CNP, self).__init__()
 
         self.input_dim = input_dim
@@ -86,12 +86,13 @@ class WTA_CNP(nn.Module):
         pred_means = pred[:, :, :, :self.output_dim]
         pred_stds = nn.functional.softplus(pred[:, :, :, self.output_dim:])
 
-        pred_dists = torch.distributions.Normal(pred_means, pred_stds)  # <num_decoders>-many gaussians
+        pred_dists = torch.distributions.Normal(pred_means, pred_stds)  # <num_decoders>-dimensional multivariate gaussian
         dec_loss = (-pred_dists.log_prob(real)).mean((-2, -1))  # (num_decoders, batch_size) - mean over tar and output_dim
 
         #############
         # Actual loss
-        nll = torch.matmul(gate_vals, dec_loss).mean()  # (batch_size, batch_size).mean() = scalar
+        weighted_nll_per_ind = torch.sum(torch.mul(gate_vals.squeeze(1), dec_loss.T), dim=-1)  # (batch_size, )
+        nll = weighted_nll_per_ind.mean()  # scalar
 
         #############
         # Doubt is defined over individual gates. We want to penalize the model for being unsure about a single prediction; i.e we want to decrease doubt --> decrease entropy