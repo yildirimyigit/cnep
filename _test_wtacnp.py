528db4962f7224cf9dd67a0199acd8b7c1abbfde
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-08-14 14:22:08 +0300

    remotely runnable script. Noise is optional in data generation

--- _test_wtacnp.py
+++ _test_wtacnp.py
@@ -1,5 +1,6 @@
 # %%
 import torch
+import matplotlib.pyplot as plt
 
 from models.wta_cnp import WTA_CNP
 from data.data_generators import *
@@ -12,10 +13,10 @@ else:
 # %%
 # Hyperparameters
 batch_size = 32
-n_max_obs, n_max_tar = 10, 10
+n_max_obs, n_max_tar = 6, 6
 
 t_steps = 200
-num_demos = 100
+num_demos = 128
 num_classes = 4
 num_indiv = num_demos//num_classes  # number of demos per class
 noise_clip = 0.0
@@ -27,33 +28,45 @@ num_val_indiv = num_val//num_classes
 colors = ['r', 'g', 'b', 'm']
 
 # %%
-# Generating the data
-x = torch.linspace(0, 1, 200).repeat(num_indiv, 1)
-y = torch.zeros(num_demos, t_steps, dy)
+from IPython import get_ipython
+ip = get_ipython()
+if ip is None:
+    ip = False
+else:
+    ip = True
 
-vx = torch.linspace(0, 1, 200).repeat(num_val_indiv, 1)
-vy = torch.zeros(num_val, t_steps, dy)
+# %%
+# Data generation
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
-generator_functions = [generate_sin, generate_cos, generate_cx_sigm, generate_reverse_cx_sigm]
+dx = 1
+t_steps = 200
+num_train_per_class, num_val_per_class = 32, 8
+num_classes = 4
 
-for i in range(num_classes):
-    noise = torch.clamp(torch.randn(x.shape)*1e-4**0.5, min=0) - noise_clip
-    y[i*num_indiv:(i+1)*num_indiv] = torch.unsqueeze(generator_functions[i](x) + noise, 2)
-    noise = torch.clamp(torch.randn(vx.shape)*1e-4**0.5, min=0) - noise_clip
-    vy[i*num_val_indiv:(i+1)*num_val_indiv] = torch.unsqueeze(generator_functions[i](vx) + noise, 2)
+x = torch.linspace(0, 1, t_steps).view(-1, 1)
 
-x = torch.unsqueeze(x.repeat(num_classes, 1), 2)  # since dx = 1
-vx = torch.unsqueeze(vx.repeat(num_classes, 1), 2)
-print("X:", x.shape, "Y:", y.shape, "VX:", vx.shape, "VY:", vy.shape)
+generator_functions = [generate_sin, generate_cos, generate_cx_sigm, 
+    generate_reverse_cx_sigm]
+y = []
+for i in range(num_train_per_class + num_val_per_class):
+    for j in range(num_classes):
+        y.append(generator_functions[j](x))
 
-# for i in range(num_indiv):
-#     plt.plot(x[i, :, 0], y[i, :, 0], 'r', alpha=0.3)
-#     plt.plot(x[i+num_indiv, :, 0], y[i+num_indiv, :, 0], 'g', alpha=0.3)
-#     plt.plot(x[i+2*num_indiv, :, 0], y[i+2*num_indiv, :, 0], 'b', alpha=0.3)
-#     plt.plot(x[i+3*num_indiv, :, 0], y[i+3*num_indiv, :, 0], 'magenta', alpha=0.3)
-# plt.show()
+if ip:
+    for i, y_i in enumerate(y):
+        plt.plot(y_i, alpha=0.5, c=colors[i%num_classes])
 
-x, y = x.to(device), y.to(device)
+x = x.unsqueeze(0).repeat(len(y), 1, 1).to(device)
+y = torch.stack(y, dim=0).to(device)
+
+vx, vy = x[num_train_per_class*num_classes:], y[num_train_per_class*num_classes:]
+x, y = x[:num_train_per_class*num_classes], y[:num_train_per_class*num_classes]
+
+if ip:
+    print(x.shape, y.shape)
+    print(vx.shape, vy.shape)
 
 # %%
 def get_batch(x, y, traj_ids):
@@ -76,20 +89,90 @@ def get_batch(x, y, traj_ids):
     # print("Obs:", obs.shape, "Tar:", tar.shape, "Tar_val:", tar_val.shape)
     return obs, tar, tar_val
 
+def get_validation_batch(vx, vy, traj_ids, device=device):
+    num_obs = torch.randint(1, n_max_obs, (1,)).item()
+
+    obs = torch.zeros(batch_size, num_obs, dx+dy, device=device)
+    tar = torch.zeros(batch_size, t_steps, dx, device=device)
+    tar_val = torch.zeros(batch_size, t_steps, dy, device=device)
+
+    for i in range(len(traj_ids)):
+        random_query_ids = torch.randperm(t_steps)
+        o_ids = random_query_ids[:num_obs]
+
+        obs[i, :, :] = torch.cat((vx[traj_ids[i], o_ids], vy[traj_ids[i], o_ids]), dim=-1)
+        tar[i, :, :] = vx[traj_ids[i]]
+        tar_val[i, :, :] = vy[traj_ids[i]]
+
+    return obs, tar, tar_val
+
 # %%
-model = WTA_CNP(1, 1, 10, 10, [128, 128, 128], 4, [128, 128], 32).to(device)
+model = WTA_CNP(1, 1, 6, 6, [256, 256], 4, [184, 184, 184], 32, nll_coef=5.0, batch_entropy_coef=-4.0, ind_entropy_coef=-1.3).to(device)
 optimizer = torch.optim.Adam(lr=1e-4, params=model.parameters())
-# model
+
+print(sum(p.numel() for p in model.parameters()))
+
+# %%
+from matplotlib.lines import Line2D
+
+def draw_val_plot(root_folder, epoch):
+    plt_y_lim = torch.max(vy) + 0.1
+
+    obs = torch.zeros((model.num_decoders, 1, 1, 2)).to(device)
+    for i in range(batch_size):
+        obs[i] = torch.Tensor([x[i, 80, 0], y[i, 80, 0]]).unsqueeze(0).unsqueeze(0).to(device)
+
+    tar = torch.linspace(0, 1, 200).unsqueeze(0).unsqueeze(-1).to(device)
+
+    with torch.no_grad():
+        for i in range(batch_size):
+            pred_wta, gate = model(obs[i], tar)
+
+            plt.ylim((-plt_y_lim, plt_y_lim))
+            plt.scatter(obs[i,:,:,0].cpu(), obs[i,:,:,1].cpu(), c='k')
+            for j in range(batch_size):
+                plt.plot(torch.linspace(0, 1, 200), pred_wta[j,0,:,0].cpu(), colors[j], alpha=max(0.2, gate[0, 0, j].item()))  # wta pred
+            handles = []
+            for j in range(batch_size):
+                plt.plot(torch.linspace(0, 1, 200), vy[j].squeeze(-1).cpu(), 'k', alpha=0.05 if j!=i else 0.35)  # data
+                handles.append(Line2D([0], [0], label=f'gate{j}: {gate[0, 0, j].item():.4f}', color=colors[j]))
+
+            plt.legend(handles=handles, loc='upper right')
+
+            plt.savefig(f'{root_folder}img/{i}_{epoch}.png')
+            plt.close()
 
 # %%
 import time
+import os
 
-file_name = int(time.time())
+timestamp = int(time.time())
+root_folder = f'outputs/combined/{str(timestamp)}/'
 
-epochs = 1000_000
+if not os.path.exists(root_folder):
+    os.makedirs(root_folder)
+
+if not os.path.exists(f'{root_folder}saved_models/'):
+    os.makedirs(f'{root_folder}saved_models/')
+
+if not os.path.exists(f'{root_folder}img/'):
+    os.makedirs(f'{root_folder}img/')
+
+torch.save(y, f'{root_folder}y.pt')
+
+epochs = 1_500_000
 epoch_iter = num_demos//batch_size  # number of batches per epoch (e.g. 100//32 = 3)
-min_loss = 1000000
+v_epoch_iter = num_val//batch_size  # number of batches per validation (e.g. 100//32 = 3)
 avg_loss = 0
+
+val_per_epoch = 1000
+min_val_error = 1000000
+
+mse_loss = torch.nn.MSELoss()
+
+training_loss, validation_error = [], []
+tr_loss_path = f'{root_folder}training_loss.pt'
+val_err_path = f'{root_folder}validation_error.pt'
 
 for epoch in range(epochs):
     epoch_loss = 0
@@ -100,15 +183,34 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         obs, tar_x, tar_y = get_batch(x, y, traj_ids[i])
         pred, gate = model(obs, tar_x)
-        loss, _ = model.loss(pred, gate, tar_y)
+        loss, nll = model.loss(pred, gate, tar_y)
         loss.backward()
         optimizer.step()
 
-        epoch_loss += loss.item()
+        epoch_loss += nll.item()
 
-    if epoch_loss < min_loss and epoch > 5e4:
-        min_loss = epoch_loss
-        torch.save(model.state_dict(), f'saved_models/wtacnp_synth_{file_name}.pt')
+    training_loss.append(epoch_loss)
+
+    if epoch % val_per_epoch == 0:
+        with torch.no_grad():
+            v_traj_ids = torch.randperm(vx.shape[0])[:batch_size*v_epoch_iter].chunk(v_epoch_iter)
+            val_epoch_err = 0
+
+            for j in range(v_epoch_iter):
+                o, t, tr = get_validation_batch(vx, vy, v_traj_ids[j])
+                p, g = model(o, t)
+                dec_id = torch.argmax(g.squeeze(1), dim=-1)
+                vp_means = p[dec_id, torch.arange(batch_size), :, :dy]
+                val_epoch_err += mse_loss(vp_means, tr).item()
+
+            validation_error.append(val_epoch_err)
+
+            if val_epoch_err < min_val_error:
+                min_val_error = val_epoch_err
+                print(f'New best: {min_val_error}')
+                torch.save(model.state_dict(), f'{root_folder}saved_models/best.pt')
+
+        draw_val_plot(root_folder, epoch)
 
     avg_loss += epoch_loss
 
@@ -116,49 +218,87 @@ for epoch in range(epochs):
         print("Epoch: {}, Loss: {}".format(epoch, avg_loss/100))
         avg_loss = 0
 
-    # if epoch % 5000 == 0:
-    #     print(gate)
+    if epoch % 100000:
+        torch.save(torch.Tensor(training_loss), tr_loss_path)
+        torch.save(torch.Tensor(validation_error), val_err_path)
+
 
 # %%
-def get_validation_batch(o_ids=[0, -1]):
-    obs = torch.cat((vx[:, o_ids, :], vy[:, o_ids, :]), dim=-1)
-    tar = vx[:, torch.arange(t_steps)]
-    tar_val= vy[:, torch.arange(t_steps)]
+# def get_validation_batch(o_ids=[0, -120]):
+#     obs = torch.cat((vx[:, o_ids, :], vy[:, o_ids, :]), dim=-1)
+#     tar = vx[:, torch.arange(t_steps)]
+#     tar_val= vy[:, torch.arange(t_steps)]
 
-    return obs, tar, tar_val
+#     return obs, tar, tar_val
 
-def generate_test_batch(n=4):
-    obs = torch.zeros(n, 2, 2)
-    obs[0] = torch.tensor([[0, 0], [1, 0]])
-    obs[1] = torch.tensor([[0, 0], [1, 1]])
-    obs[2] = torch.tensor([[0, 1], [1, 0]])
-    obs[3] = torch.tensor([[0, 1], [1, 1]])
-    tar = vx[:4, torch.arange(t_steps)]
+# def get_validation_batch(o_ids=[15]):
+#     obs = torch.cat((vx[:, o_ids, :], vy[:, o_ids, :]), dim=-1)
+#     tar = vx[:, torch.arange(t_steps)]
+#     tar_val= vy[:, torch.arange(t_steps)]
 
-    return obs, tar
+#     return obs, tar, tar_val
 
-# %%
-# file_name = '1687358495'
+# def generate_test_batch(n=4):
+#     obs = torch.zeros(n, 2, 2)
+#     obs[0] = torch.tensor([[0, 0], [1, 0]])
+#     obs[1] = torch.tensor([[0, 0], [1, 1]])
+#     obs[2] = torch.tensor([[0, 1], [1, 0]])
+#     obs[3] = torch.tensor([[0, 1], [1, 1]])
+#     tar = vx[:4, torch.arange(t_steps)]
 
-# Testing the best model
-model = WTA_CNP(1, 1, 10, 10, [128, 128, 128], 4, [128, 128], 32)
-model.load_state_dict(torch.load(f'saved_models/wtacnp_synth_{file_name}.pt'))
-model.eval()
-
-o, t, tr = get_validation_batch()
-
-with torch.no_grad():
-    p, g = model(o, t)
-
-p, g = p.cpu().numpy(), g.cpu()
-t, tr = t.cpu().numpy(), tr.cpu().numpy()
-# %%
-pytorch_total_params = sum(p.numel() for p in model.parameters())
-print(pytorch_total_params)
-pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(pytorch_total_params)
+#     return obs, tar
 
 # %%
+# file_name = '1691856202'
 
+# # Testing the best model
+# model = WTA_CNP(1, 1, 6, 6, [128, 256, 128], 4, [128, 128, 128], 32)
+# model.load_state_dict(torch.load(f'saved_models/wtacnp_synth_{file_name}.pt'))
+# model.eval()
+
+# o, t, tr = get_validation_batch()
+# print(o.shape, t.shape, tr.shape)
+
+# with torch.no_grad():
+#     p, g = model(o.cpu(), t.cpu())
+
+# p, g = p.cpu().numpy(), g.cpu()
+# t, tr = t.cpu().numpy(), tr.cpu().numpy()
+
+# for i in range(batch_size):
+#     dec_id = torch.argmax(g[i, :, :]).item()
+#     plt.plot(range(t_steps), p[dec_id, i, :, 0], colors[dec_id], alpha=0.3)
+#     plt.plot(range(t_steps), tr[i, :, 0], 'k', alpha=0.3, linestyle='dashed')
+
+# print(g.mean(dim=0))
+
+# %%
+# model = WTA_CNP(1, 1, 10, 10, [128, 128, 128], 4, [128, 128, 128], 4)
+# model.load_state_dict(torch.load(f'saved_models/wtacnp_synth_{file_name}.pt'))
+# model.eval()
+
+# o, t = generate_test_batch()
+
+# print(o)
+
+# with torch.no_grad():
+#     p, g = model(o.cpu(), t.cpu())
+
+# p, g = p.cpu().numpy(), g.cpu()
+
+# for i in range(4):
+
+#     print(o[i, 0, 0], o[i, 0, 1], o[i, 1, 0], o[i, 1, 1])
+
+#     dec_id = torch.argmax(g[i, :, :]).item()
+#     plt.plot(range(t_steps), p[dec_id, i, :, 0], colors[dec_id], alpha=0.7)
+
+#     plt.scatter(o[i, 0, 0]*200, o[i, 0, 1], color='k', marker='x')
+#     plt.scatter(o[i, 1, 0]*200, o[i, 1, 1], color='k', marker='x')
+
+# for i in range(32):
+#     plt.plot(range(t_steps), tr[i, :, 0], 'k', alpha=0.1, linestyle='dashed')
+
+# plt.show()
 
 
--- data/data_generators.py
+++ data/data_generators.py
@@ -1,17 +1,20 @@
 import torch
 
-def generate_cx_sigm(x:torch.Tensor):
+def generate_cx_sigm(x:torch.Tensor, noisy=False):
     # rand_btw_7-30 * {-1, 1}
     c1 = torch.rand_like(x) * 1 + 7 #* torch.from_numpy(np.random.choice([-1, 1], (n, 1)))  # c1 for steepness
     c2 = 0.5  # c2 midpoint
 
-    noise = torch.clamp(torch.randn_like(x)*1e-4**0.5, min=0)
+    noise = 0
+    if noisy:
+        noise = torch.clamp(torch.randn_like(x)*1e-4**0.5, min=0)
+        
     data = 1/(1 + torch.exp(-c1 * (x-c2))) + noise
     return data
 
 
-def generate_reverse_cx_sigm(x:torch.Tensor):
-    return 1-generate_cx_sigm(x)
+def generate_reverse_cx_sigm(x:torch.Tensor, noisy=False):
+    return 1-generate_cx_sigm(x, noisy)
 
 
 def generate_sin(x:torch.Tensor, noisy=False):
@@ -27,7 +30,7 @@ def generate_cos(x:torch.Tensor, noisy=False):
     noise = 0
     if noisy:
         noise = torch.clamp(torch.randn_like(x)*1e-4**0.5, min=0).view(-1, dx)
-    return torch.cos(x*torch.pi) + noise
+    return torch.cos((x+0.5)*torch.pi) + 1 + noise
 
 def generate_chainsaw(x:torch.Tensor, amp=1, mod=0.5, eps=0.1):
     r1 = torch.rand(1)*(eps*2)+(1-eps)
