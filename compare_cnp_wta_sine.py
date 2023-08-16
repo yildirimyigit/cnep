3e67e90cec338548a8beeeefa7fab08df44c0a79
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-08-17 02:36:27 +0300

    remotely runnable script

--- compare_cnp_wta_sine.py
+++ compare_cnp_wta_sine.py
@@ -196,114 +196,115 @@ def draw_val_plot(root_folder, epoch):
 import time
 import os
 
-timestamp = int(time.time())
-root_folder = f'outputs/sine/{str(timestamp)}/'
+for _ in range(10):
+    timestamp = int(time.time())
+    root_folder = f'outputs/sine/{str(timestamp)}/'
 
-if not os.path.exists(root_folder):
-    os.makedirs(root_folder)
+    if not os.path.exists(root_folder):
+        os.makedirs(root_folder)
 
-if not os.path.exists(f'{root_folder}saved_models/'):
-    os.makedirs(f'{root_folder}saved_models/')
+    if not os.path.exists(f'{root_folder}saved_models/'):
+        os.makedirs(f'{root_folder}saved_models/')
 
-if not os.path.exists(f'{root_folder}img/'):
-    os.makedirs(f'{root_folder}img/')
+    if not os.path.exists(f'{root_folder}img/'):
+        os.makedirs(f'{root_folder}img/')
 
-torch.save(y, f'{root_folder}y.pt')
-
-
-epochs = 2_000_000
-epoch_iter = num_demos//batch_size  # number of batches per epoch (e.g. 100//32 = 3)
-v_epoch_iter = num_val//batch_size  # number of batches per validation (e.g. 100//32 = 3)
-avg_loss_wta, avg_loss_cnp = 0, 0
-
-val_per_epoch = 1000
-min_val_loss_wta, min_val_loss_cnp = 1000000, 1000000
-
-mse_loss = torch.nn.MSELoss()
-
-training_loss_wta, validation_error_wta = [], []
-training_loss_cnp, validation_error_cnp = [], []
-
-wta_tr_loss_path = f'{root_folder}wta_training_loss.pt'
-wta_val_err_path = f'{root_folder}wta_validation_error.pt'
-cnp_tr_loss_path = f'{root_folder}cnp_training_loss.pt'
-cnp_val_err_path = f'{root_folder}cnp_validation_error.pt'
-
-for epoch in range(epochs):
-    epoch_loss_wta, epoch_loss_cnp = 0, 0
-
-    traj_ids = torch.randperm(x.shape[0])[:batch_size*epoch_iter].chunk(epoch_iter)  # [:batch_size*epoch_iter] because nof_trajectories may be indivisible by batch_size
-
-    for i in range(epoch_iter):
-        optimizer_wta.zero_grad()
-        optimizer_cnp.zero_grad()
-
-        obs_wta, tar_x_wta, tar_y_wta = get_batch(x, y, traj_ids[i], device_wta)
-        obs_cnp, tar_x_cnp, tar_y_cnp = obs_wta.clone(), tar_x_wta.clone(), tar_y_wta.clone()
-
-        pred_wta, gate_wta = model_wta(obs_wta, tar_x_wta)
-        pred_cnp, encoded_rep_cnp = model_cnp(obs_cnp, tar_x_cnp)
-
-        loss_wta, wta_nll = model_wta.loss(pred_wta, gate_wta, tar_y_wta)
-
-        loss_wta.backward()
-        optimizer_wta.step()
-
-        loss_cnp = model_cnp.loss(pred_cnp, tar_y_cnp)
-        loss_cnp.backward()
-        optimizer_cnp.step()
-
-        epoch_loss_wta += wta_nll.item()
-        epoch_loss_cnp += loss_cnp.item()
-
-    training_loss_wta.append(epoch_loss_wta)
-    training_loss_cnp.append(epoch_loss_cnp)
-
-    if epoch % val_per_epoch == 0:
-        with torch.no_grad():
-            v_traj_ids = torch.randperm(vx.shape[0])[:batch_size*v_epoch_iter].chunk(v_epoch_iter)
-            val_loss_wta, val_loss_cnp = 0, 0
-
-            for j in range(v_epoch_iter):
-                o_wta, t_wta, tr_wta = get_validation_batch(vx, vy, v_traj_ids[j], device=device_wta)
-                o_cnp, t_cnp, tr_cnp = o_wta.clone(), t_wta.clone(), tr_wta.clone()
-
-                p_wta, g_wta = model_wta(o_wta, t_wta)
-                dec_id = torch.argmax(g_wta.squeeze(1), dim=-1)
-                vp_means = p_wta[dec_id, torch.arange(batch_size), :, :dy]
-                val_loss_wta += mse_loss(vp_means, tr_wta).item()
-
-                pred_cnp, encoded_rep = model_cnp(o_cnp, t_cnp)
-                val_loss_cnp += mse_loss(pred_cnp[:, :, :model_cnp.output_dim], tr_cnp)
+    torch.save(y, f'{root_folder}y.pt')
 
 
-            validation_error_wta.append(val_loss_wta)
-            if val_loss_wta < min_val_loss_wta:
-                min_val_loss_wta = val_loss_wta
-                print(f'(WTA)New best: {min_val_loss_wta}')
-                torch.save(model_wta.state_dict(), f'{root_folder}saved_models/wta_on_synth.pt')
+    epochs = 2_000_000
+    epoch_iter = num_demos//batch_size  # number of batches per epoch (e.g. 100//32 = 3)
+    v_epoch_iter = num_val//batch_size  # number of batches per validation (e.g. 100//32 = 3)
+    avg_loss_wta, avg_loss_cnp = 0, 0
 
-            validation_error_cnp.append(val_loss_cnp.item())
-            if val_loss_cnp < min_val_loss_cnp:
-                min_val_loss_cnp = val_loss_cnp
-                print(f'(CNP)New best: {min_val_loss_cnp}')
-                torch.save(model_cnp.state_dict(), f'{root_folder}saved_models/cnp_on_synth.pt')
-  
-        # draw_val_plot(root_folder, epoch)
+    val_per_epoch = 1000
+    min_val_loss_wta, min_val_loss_cnp = 1000000, 1000000
+
+    mse_loss = torch.nn.MSELoss()
+
+    training_loss_wta, validation_error_wta = [], []
+    training_loss_cnp, validation_error_cnp = [], []
+
+    wta_tr_loss_path = f'{root_folder}wta_training_loss.pt'
+    wta_val_err_path = f'{root_folder}wta_validation_error.pt'
+    cnp_tr_loss_path = f'{root_folder}cnp_training_loss.pt'
+    cnp_val_err_path = f'{root_folder}cnp_validation_error.pt'
+
+    for epoch in range(epochs):
+        epoch_loss_wta, epoch_loss_cnp = 0, 0
+
+        traj_ids = torch.randperm(x.shape[0])[:batch_size*epoch_iter].chunk(epoch_iter)  # [:batch_size*epoch_iter] because nof_trajectories may be indivisible by batch_size
+
+        for i in range(epoch_iter):
+            optimizer_wta.zero_grad()
+            optimizer_cnp.zero_grad()
+
+            obs_wta, tar_x_wta, tar_y_wta = get_batch(x, y, traj_ids[i], device_wta)
+            obs_cnp, tar_x_cnp, tar_y_cnp = obs_wta.clone(), tar_x_wta.clone(), tar_y_wta.clone()
+
+            pred_wta, gate_wta = model_wta(obs_wta, tar_x_wta)
+            pred_cnp, encoded_rep_cnp = model_cnp(obs_cnp, tar_x_cnp)
+
+            loss_wta, wta_nll = model_wta.loss(pred_wta, gate_wta, tar_y_wta)
+
+            loss_wta.backward()
+            optimizer_wta.step()
+
+            loss_cnp = model_cnp.loss(pred_cnp, tar_y_cnp)
+            loss_cnp.backward()
+            optimizer_cnp.step()
+
+            epoch_loss_wta += wta_nll.item()
+            epoch_loss_cnp += loss_cnp.item()
+
+        training_loss_wta.append(epoch_loss_wta)
+        training_loss_cnp.append(epoch_loss_cnp)
+
+        if epoch % val_per_epoch == 0:
+            with torch.no_grad():
+                v_traj_ids = torch.randperm(vx.shape[0])[:batch_size*v_epoch_iter].chunk(v_epoch_iter)
+                val_loss_wta, val_loss_cnp = 0, 0
+
+                for j in range(v_epoch_iter):
+                    o_wta, t_wta, tr_wta = get_validation_batch(vx, vy, v_traj_ids[j], device=device_wta)
+                    o_cnp, t_cnp, tr_cnp = o_wta.clone(), t_wta.clone(), tr_wta.clone()
+
+                    p_wta, g_wta = model_wta(o_wta, t_wta)
+                    dec_id = torch.argmax(g_wta.squeeze(1), dim=-1)
+                    vp_means = p_wta[dec_id, torch.arange(batch_size), :, :dy]
+                    val_loss_wta += mse_loss(vp_means, tr_wta).item()
+
+                    pred_cnp, encoded_rep = model_cnp(o_cnp, t_cnp)
+                    val_loss_cnp += mse_loss(pred_cnp[:, :, :model_cnp.output_dim], tr_cnp)
 
 
-    avg_loss_wta += epoch_loss_wta
-    avg_loss_cnp += epoch_loss_cnp
+                validation_error_wta.append(val_loss_wta)
+                if val_loss_wta < min_val_loss_wta:
+                    min_val_loss_wta = val_loss_wta
+                    print(f'(WTA)New best: {min_val_loss_wta}')
+                    torch.save(model_wta.state_dict(), f'{root_folder}saved_models/wta_on_synth.pt')
 
-    if epoch % 100 == 0:
-        print("Epoch: {}, WTA-Loss: {}, CNP-Loss: {}".format(epoch, avg_loss_wta/100, avg_loss_cnp/100))
-        avg_loss_wta, avg_loss_cnp = 0, 0
+                validation_error_cnp.append(val_loss_cnp.item())
+                if val_loss_cnp < min_val_loss_cnp:
+                    min_val_loss_cnp = val_loss_cnp
+                    print(f'(CNP)New best: {min_val_loss_cnp}')
+                    torch.save(model_cnp.state_dict(), f'{root_folder}saved_models/cnp_on_synth.pt')
+    
+            # draw_val_plot(root_folder, epoch)
 
-    if epoch % 100000:
-        torch.save(torch.Tensor(training_loss_wta), wta_tr_loss_path)
-        torch.save(torch.Tensor(validation_error_wta), wta_val_err_path)
-        torch.save(torch.Tensor(training_loss_cnp), cnp_tr_loss_path)
-        torch.save(torch.Tensor(validation_error_cnp), cnp_val_err_path)
+
+        avg_loss_wta += epoch_loss_wta
+        avg_loss_cnp += epoch_loss_cnp
+
+        if epoch % 100 == 0:
+            print("Epoch: {}, WTA-Loss: {}, CNP-Loss: {}".format(epoch, avg_loss_wta/100, avg_loss_cnp/100))
+            avg_loss_wta, avg_loss_cnp = 0, 0
+
+        if epoch % 100000:
+            torch.save(torch.Tensor(training_loss_wta), wta_tr_loss_path)
+            torch.save(torch.Tensor(validation_error_wta), wta_val_err_path)
+            torch.save(torch.Tensor(training_loss_cnp), cnp_tr_loss_path)
+            torch.save(torch.Tensor(validation_error_cnp), cnp_val_err_path)
 
 # %%
 # get_batch(x, y, torch.tensor([0]))
