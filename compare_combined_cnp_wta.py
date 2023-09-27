92c34ab2ea325647883017c3e7aa42b86f5db2c9
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-09-27 19:03:31 +0300

    compiled save causes troubles. More details: https://discuss.pytorch.org/t/how-to-save-load-a-model-with-torch-compile/179739

--- compare_combined_cnp_wta.py
+++ compare_combined_cnp_wta.py
@@ -10,9 +10,9 @@ def get_available_gpu_with_most_memory():
     for i in range(torch.cuda.device_count()):
         torch.cuda.set_device(i)  # Switch to the GPU to accurately measure memory
         gpu_memory.append((i, torch.cuda.memory_stats()['reserved_bytes.all.current'] / (1024 ** 2)))
-    
+
     gpu_memory.sort(key=lambda x: x[1], reverse=True)
-    
+
     return gpu_memory[0][0]
 
 if torch.cuda.is_available():
@@ -76,14 +76,14 @@ x1, y1 = x.to(device_cnp), y.to(device_cnp)
 def get_batch(x, y, traj_ids, device=device_wta):
     n_o = torch.randint(1, n_max_obs, (1,)).item()
     n_t = torch.randint(1, n_max_tar, (1,)).item()
-    
+
     tar = torch.zeros(batch_size, n_t, dx, device=device)
     tar_val = torch.zeros(batch_size, n_t, dy, device=device)
     obs = torch.zeros(batch_size, n_o, dx+dy, device=device)
 
     for i in range(len(traj_ids)):
         random_query_ids = torch.randperm(t_steps)
-        
+
         o_ids = random_query_ids[:n_o]
         t_ids = random_query_ids[n_o:n_o+n_t]
 
@@ -114,20 +114,18 @@ def get_validation_batch(vx, vy, traj_ids, device=device_wta):
 import time
 import os
 
-for _ in range(5):
+for _ in range(10):
+    model_wta_ = WTA_CNP(1, 1, n_max_obs, n_max_tar, [256, 256, 256], num_decoders=4, decoder_hidden_dims=[256, 256, 256], batch_size=batch_size, scale_coefs=True).to(device_wta)
+    optimizer_wta = torch.optim.Adam(lr=1e-4, params=model_wta_.parameters())
 
-# %%
-    model_wta = WTA_CNP(1, 1, n_max_obs, n_max_tar, [185, 185, 185], num_decoders=4, decoder_hidden_dims=[256, 256], batch_size=batch_size, scale_coefs=True).to(device_wta)
-    optimizer_wta = torch.optim.Adam(lr=1e-4, params=model_wta.parameters())
-
-    model_cnp = CNP(input_dim=1, hidden_dim=256, output_dim=1, n_max_obs=n_max_obs, n_max_tar=n_max_tar, num_layers=3, batch_size=batch_size).to(device_cnp)
-    optimizer_cnp = torch.optim.Adam(lr=1e-4, params=model_cnp.parameters())
+    model_cnp_ = CNP(input_dim=1, hidden_dim=406, output_dim=1, n_max_obs=n_max_obs, n_max_tar=n_max_tar, num_layers=3, batch_size=batch_size).to(device_cnp)
+    optimizer_cnp = torch.optim.Adam(lr=1e-4, params=model_cnp_.parameters())
 
     if torch.__version__ >= "2.0":
-        model_cnp, model_wta = torch.compile(model_cnp), torch.compile(model_wta)
+        model_cnp, model_wta = torch.compile(model_cnp_), torch.compile(model_wta_)
 
     timestamp = int(time.time())
-    root_folder = f'outputs/combined/fin/{str(timestamp)}/'
+    root_folder = f'outputs/combined/large/{str(timestamp)}/'
 
     if not os.path.exists(root_folder):
         os.makedirs(root_folder)
@@ -141,18 +139,21 @@ for _ in range(5):
     torch.save(y, f'{root_folder}y.pt')
 
 
-    epochs = 1_500_000
+    epochs = 5_000_000
     epoch_iter = num_demos//batch_size  # number of batches per epoch (e.g. 100//32 = 3)
     v_epoch_iter = num_val//batch_size
     avg_loss_wta, avg_loss_cnp = 0, 0
 
+    print_loss_per_epoch = 1000
     val_per_epoch = 1000
     min_val_loss_wta, min_val_loss_cnp = 1000000, 1000000
 
     mse_loss = torch.nn.MSELoss()
 
-    training_loss_wta, validation_error_wta = [], []
-    training_loss_cnp, validation_error_cnp = [], []
+
+    training_loss_cnp, validation_error_cnp = torch.zeros(epochs, device=device_cnp), torch.zeros(epochs//val_per_epoch, device=device_cnp)
+    training_loss_wta, validation_error_wta = torch.zeros(epochs, device=device_wta), torch.zeros(epochs//val_per_epoch, device=device_wta)
+    validation_ind = 0
 
     wta_tr_loss_path = f'{root_folder}wta_training_loss.pt'
     wta_val_err_path = f'{root_folder}wta_validation_error.pt'
@@ -186,8 +187,8 @@ for _ in range(5):
             epoch_loss_wta += wta_nll.item()
             epoch_loss_cnp += loss_cnp.item()
 
-        training_loss_wta.append(epoch_loss_wta)
-        training_loss_cnp.append(epoch_loss_cnp)
+        training_loss_wta[i] = epoch_loss_wta
+        training_loss_cnp[i] = epoch_loss_cnp
 
         if epoch % val_per_epoch == 0:
             with torch.no_grad():
@@ -206,23 +207,25 @@ for _ in range(5):
                     pred_cnp, encoded_rep = model_cnp(o_cnp, t_cnp)
                     val_loss_cnp += mse_loss(pred_cnp[:, :, :model_cnp.output_dim], tr_cnp)
 
-                validation_error_wta.append(val_loss_wta)
+                validation_error_wta[validation_ind] = val_loss_wta
                 if val_loss_wta < min_val_loss_wta:
                     min_val_loss_wta = val_loss_wta
                     print(f'(WTA)New best: {min_val_loss_wta}')
-                    torch.save(model_wta.state_dict(), f'{root_folder}saved_models/wta_on_synth.pt')
+                    torch.save(model_wta_.state_dict(), f'{root_folder}saved_models/wta_on_synth.pt')
 
-                validation_error_cnp.append(val_loss_cnp.item())
+                validation_error_cnp[validation_ind] = val_loss_cnp
                 if val_loss_cnp < min_val_loss_cnp:
                     min_val_loss_cnp = val_loss_cnp
                     print(f'(CNP)New best: {min_val_loss_cnp}')
-                    torch.save(model_cnp.state_dict(), f'{root_folder}saved_models/cnp_on_synth.pt')
+                    torch.save(model_cnp_.state_dict(), f'{root_folder}saved_models/cnp_on_synth.pt')
+
+                validation_ind += 1
 
         avg_loss_wta += epoch_loss_wta
         avg_loss_cnp += epoch_loss_cnp
 
-        if epoch % 100 == 0:
-            print("Epoch: {}, WTA-Loss: {}, CNP-Loss: {}".format(epoch, avg_loss_wta/100, avg_loss_cnp/100))
+        if epoch % print_loss_per_epoch == 0:
+            print("Epoch: {}, WTA-Loss: {}, CNP-Loss: {}".format(epoch, avg_loss_wta/print_loss_per_epoch, avg_loss_cnp/print_loss_per_epoch))
             avg_loss_wta, avg_loss_cnp = 0, 0
 
     torch.save(torch.Tensor(training_loss_wta), wta_tr_loss_path)
@@ -233,5 +236,6 @@ for _ in range(5):
 
     # %%
     open(f'{root_folder}fin', 'w').close()
+    print('=======================================')
 
 
