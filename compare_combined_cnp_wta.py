e087db65d1c56c50a94adf4945a659f35c5b0477
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-10-07 22:59:29 +0300

    minor: folder name

--- compare_combined_cnp_wta.py
+++ compare_combined_cnp_wta.py
@@ -125,7 +125,7 @@ for _ in range(5):
         model_cnp, model_wta = torch.compile(model_cnp_), torch.compile(model_wta_)
 
     timestamp = int(time.time())
-    root_folder = f'outputs/combined/last_decision/{str(timestamp)}/'
+    root_folder = f'outputs/combined/last_last/{str(timestamp)}/'
 
     if not os.path.exists(root_folder):
         os.makedirs(root_folder)
