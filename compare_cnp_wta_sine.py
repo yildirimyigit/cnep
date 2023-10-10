b2fe2bb24ad2cc93020236e8bc6b8be7a6f82f45
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2023-10-10 15:08:49 +0300

    minor updates

--- compare_cnp_wta_sine.py
+++ compare_cnp_wta_sine.py
@@ -109,12 +109,12 @@ def get_validation_batch(vx, vy, traj_ids, device=device_wta):
 import time
 import os
 
-for _ in range(20):
+for _ in range(2):
 
-    model_wta_ = WTA_CNP(1, 1, n_max_obs, n_max_tar, [256, 256, 256], num_decoders=10, decoder_hidden_dims=[128, 128, 128], batch_size=batch_size, scale_coefs=True).to(device_wta)
+    model_wta_ = WTA_CNP(1, 1, n_max_obs, n_max_tar, [256, 256, 256], num_decoders=10, decoder_hidden_dims=[256, 256, 256], batch_size=batch_size, scale_coefs=True).to(device_wta)
     optimizer_wta = torch.optim.Adam(lr=1e-4, params=model_wta_.parameters())
 
-    model_cnp_ = CNP(input_dim=1, hidden_dim=324, output_dim=1, n_max_obs=n_max_obs, n_max_tar=n_max_tar, num_layers=4, batch_size=batch_size).to(device_cnp)
+    model_cnp_ = CNP(input_dim=1, hidden_dim=493, output_dim=1, n_max_obs=n_max_obs, n_max_tar=n_max_tar, num_layers=4, batch_size=batch_size).to(device_cnp)
     optimizer_cnp = torch.optim.Adam(lr=1e-4, params=model_cnp_.parameters())
 
     if torch.__version__ >= "2.0":
@@ -135,7 +135,7 @@ for _ in range(20):
     torch.save(y, f'{root_folder}y.pt')
 
 
-    epochs = 10_000_000
+    epochs = 20_000_000
     epoch_iter = num_demos//batch_size  # number of batches per epoch (e.g. 100//32 = 3)
     v_epoch_iter = num_val//batch_size  # number of batches per validation (e.g. 100//32 = 3)
     avg_loss_wta, avg_loss_cnp = 0, 0
