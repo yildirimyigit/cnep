c52a2bfd1a7896472418c32d4c7b82b8bb19db85
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2024-04-30 14:41:43 +0300

    updated to use CNEP instead of WTA

--- compare_cneps_on_4_sines.py
+++ compare_cneps_on_4_sines.py
@@ -1,6 +1,6 @@
 # %%
 from models.cnp import CNP
-from models.wta_cnp import WTA_CNP
+from models.cnep import CNEP
 
 from data.data_generators import *
 import torch
@@ -28,17 +28,17 @@ else:
 print("Device :", device)
 
 # %%
-batch_size = 4
-n_max_obs, n_max_tar = 10, 10
+batch_size = 32
+n_max, m_max = 10, 10  # max number of points in context set and target set
 
 t_steps = 200
-num_demos = 32
+num_demos = 128
 num_classes = 4
 num_indiv = num_demos//num_classes  # number of demos per class
 noise_clip = 0.0
 dx, dy = 1, 1
 
-num_val = 8
+num_val = 32
 num_val_indiv = num_val//num_classes
 
 # %%
@@ -85,51 +85,72 @@ print("X:", x.shape, "Y:", y.shape, "VX:", vx.shape, "VY:", vy.shape)
 # x1, y1 = x.to(device_cnp), y.to(device_cnp)
 
 # %%
-def get_batch(x, y, traj_ids, device=device):
-    n_o = torch.randint(1, n_max_obs, (1,)).item()
-    n_t = torch.randint(1, n_max_tar, (1,)).item()
-    
-    tar = torch.zeros(batch_size, n_t, dx, device=device)
-    tar_val = torch.zeros(batch_size, n_t, dy, device=device)
-    obs = torch.zeros(batch_size, n_o, dx+dy, device=device)
+obs = torch.zeros((batch_size, n_max, dx+dy), dtype=torch.float32, device=device)
+tar_x = torch.zeros((batch_size, m_max, dx), dtype=torch.float32, device=device)
+tar_y = torch.zeros((batch_size, m_max, dy), dtype=torch.float32, device=device)
+obs_mask = torch.zeros((batch_size, n_max), dtype=torch.bool, device=device)
+tar_mask = torch.zeros((batch_size, m_max), dtype=torch.bool, device=device)
 
-    for i in range(len(traj_ids)):
-        random_query_ids = torch.randperm(t_steps)
+def prepare_masked_batch(t: list, traj_ids: list):
+    obs.fill_(0)
+    tar_x.fill_(0)
+    tar_y.fill_(0)
+    obs_mask.fill_(False)
+    tar_mask.fill_(False)
+
+    for i, traj_id in enumerate(traj_ids):
+        traj = t[traj_id]
+
+        n = torch.randint(1, n_max, (1,)).item()
+        m = torch.randint(1, m_max, (1,)).item()
+
+        permuted_ids = torch.randperm(t_steps)
+        n_ids = permuted_ids[:n]
+        m_ids = permuted_ids[n:n+m]
         
-        o_ids = random_query_ids[:n_o]
-        t_ids = random_query_ids[n_o:n_o+n_t]
+        obs[i, :n, :dx] = traj[n_ids]
+        obs[i, :n, dx:] = (n_ids/t_steps).unsqueeze(1)
+        obs_mask[i, :n] = True
+        
+        tar_x[i, :m] = traj[m_ids]
+        tar_y[i, :m] = (m_ids/t_steps).unsqueeze(1)
+        tar_mask[i, :m] = True
 
-        obs[i, :, :] = torch.cat((x[traj_ids[i], o_ids], y[traj_ids[i], o_ids]), dim=-1)
-        tar[i, :, :] = x[traj_ids[i], t_ids]
-        tar_val[i, :, :] = y[traj_ids[i], t_ids]
+val_obs = torch.zeros((batch_size, n_max, dx+dy), dtype=torch.float32, device=device)
+val_tar_x = torch.zeros((batch_size, t_steps, dx), dtype=torch.float32, device=device)
+val_tar_y = torch.zeros((batch_size, t_steps, dy), dtype=torch.float32, device=device)
+val_obs_mask = torch.zeros((batch_size, n_max), dtype=torch.bool, device=device)
 
-    return obs, tar, tar_val
+def prepare_masked_val_batch(t: list, traj_ids: list):
+    val_obs.fill_(0)
+    val_tar_x.fill_(0)
+    val_tar_y.fill_(0)
+    val_obs_mask.fill_(False)
 
-def get_validation_batch(vx, vy, traj_ids, device=device):
-    num_obs = torch.randint(1, n_max_obs, (1,)).item()
+    for i, traj_id in enumerate(traj_ids):
+        traj = t[traj_id]
 
-    obs = torch.zeros(batch_size, num_obs, dx+dy, device=device)
-    tar = torch.zeros(batch_size, t_steps, dx, device=device)
-    tar_val = torch.zeros(batch_size, t_steps, dy, device=device)
+        n = torch.randint(1, n_max, (1,)).item()
 
-    for i in range(len(traj_ids)):
-        random_query_ids = torch.randperm(t_steps)
-        o_ids = random_query_ids[:num_obs]
-
-        obs[i, :, :] = torch.cat((vx[traj_ids[i], o_ids], vy[traj_ids[i], o_ids]), dim=-1)
-        tar[i, :, :] = vx[traj_ids[i]]
-        tar_val[i, :, :] = vy[traj_ids[i]]
-
-    return obs, tar, tar_val
+        permuted_ids = torch.randperm(t_steps)
+        n_ids = permuted_ids[:n]
+        m_ids = torch.arange(t_steps)
+        
+        val_obs[i, :n, :dx] = traj[n_ids]
+        val_obs[i, :n, dx:] = (n_ids/t_steps).unsqueeze(1)
+        val_obs_mask[i, :n] = True
+        
+        val_tar_x[i] = traj[m_ids]
+        val_tar_y[i] = (m_ids/t_steps).unsqueeze(1)
 
 # %%
-model2 = WTA_CNP(1, 1, n_max_obs, n_max_tar, [128,128,128], num_decoders=2, decoder_hidden_dims=[306,306,306], batch_size=batch_size, scale_coefs=True).to(device)
+model2 = CNEP(1, 1, n_max, n_max, [128,128,128], num_decoders=2, decoder_hidden_dims=[306,306,306], batch_size=batch_size, scale_coefs=True).to(device)
 optimizer2 = torch.optim.Adam(lr=1e-4, params=model2.parameters())
 
-model4 = WTA_CNP(1, 1, n_max_obs, n_max_tar, [128,128,128], num_decoders=4, decoder_hidden_dims=[201,201,201], batch_size=batch_size, scale_coefs=True).to(device)
+model4 = CNEP(1, 1, n_max, n_max, [128,128,128], num_decoders=4, decoder_hidden_dims=[201,201,201], batch_size=batch_size, scale_coefs=True).to(device)
 optimizer4 = torch.optim.Adam(lr=1e-4, params=model4.parameters())
 
-model8 = WTA_CNP(1, 1, n_max_obs, n_max_tar, [128,128,128], num_decoders=8, decoder_hidden_dims=[128,128,128], batch_size=batch_size, scale_coefs=True).to(device)
+model8 = CNEP(1, 1, n_max, n_max, [128,128,128], num_decoders=8, decoder_hidden_dims=[128,128,128], batch_size=batch_size, scale_coefs=True).to(device)
 optimizer8 = torch.optim.Adam(lr=1e-4, params=model8.parameters())
 
 def get_parameter_count(model):
@@ -174,8 +195,8 @@ mse_loss = torch.nn.MSELoss()
 tl2, tl4, tl8 = [], [], []
 ve2, ve4, ve8 = [], [], []
 
-# wta_tr_loss_path = f'{root_folder}wta_training_loss.pt'
-# wta_val_err_path = f'{root_folder}wta_validation_error.pt'
+cnep_tl_path = f'{root_folder}cnep_training_loss.pt'
+cnep_ve_path = f'{root_folder}cnep_validation_error.pt'
 
 for epoch in range(epochs):
     epoch_loss2, epoch_loss4, epoch_loss8 = 0, 0, 0
@@ -183,28 +204,35 @@ for epoch in range(epochs):
     traj_ids = torch.randperm(x.shape[0])[:batch_size*epoch_iter].chunk(epoch_iter)  # [:batch_size*epoch_iter] because nof_trajectories may be indivisible by batch_size
 
     for i in range(epoch_iter):
+        prepare_masked_batch(x, traj_ids[i])
+
         optimizer2.zero_grad()
-
-        obs, tar_x, tar_y = get_batch(x, y, traj_ids[i], device)
-
-        pred2, gate2 = model2(obs, tar_x)
-        loss2, nll2 = model2.loss(pred2, gate2, tar_y)
+        pred2, gate2 = model2(obs, tar_x, obs_mask)
+        loss2, nll2 = model2.loss(pred2, gate2, tar_y, tar_mask)
         loss2.backward()
         optimizer2.step()
 
-        pred4, gate4 = model4(obs, tar_x)
-        loss4, nll4 = model4.loss(pred4, gate4, tar_y)
+
+        optimizer4.zero_grad()
+        pred4, gate4 = model4(obs, tar_x, obs_mask)
+        loss4, nll4 = model4.loss(pred4, gate4, tar_y, tar_mask)
         loss4.backward()
         optimizer4.step()
 
-        pred8, gate8 = model8(obs, tar_x)
-        loss8, nll8 = model8.loss(pred8, gate8, tar_y)
+
+        optimizer8.zero_grad()
+        pred8, gate8 = model8(obs, tar_x, obs_mask)
+        loss8, nll8 = model8.loss(pred8, gate8, tar_y, tar_mask)
         loss8.backward()
         optimizer8.step()
 
         epoch_loss2 += nll2.item()
         epoch_loss4 += nll4.item()
         epoch_loss8 += nll8.item()
+
+    epoch_loss2 = epoch_loss2/epoch_iter
+    epoch_loss4 = epoch_loss4/epoch_iter
+    epoch_loss8 = epoch_loss8/epoch_iter
 
     tl2.append(epoch_loss2)
     tl4.append(epoch_loss4)
@@ -216,22 +244,22 @@ for epoch in range(epochs):
             val_loss2, val_loss4, val_loss8 = 0, 0, 0
 
             for j in range(v_epoch_iter):
-                o_wta, t_wta, tr_wta = get_validation_batch(vx, vy, v_traj_ids[j], device=device)
+                prepare_masked_val_batch(vx, v_traj_ids[j])
 
-                p_wta, g_wta = model2(o_wta, t_wta)
+                p_wta, g_wta = model2(val_obs, val_tar_x, val_obs_mask)
                 dec_id = torch.argmax(g_wta.squeeze(1), dim=-1)
                 vp_means = p_wta[dec_id, torch.arange(batch_size), :, :dy]
-                val_loss2 += mse_loss(vp_means, tr_wta).item()
+                val_loss2 += mse_loss(vp_means, val_tar_y).item()
 
-                p_wta, g_wta = model4(o_wta, t_wta)
+                p_wta, g_wta = model4(val_obs, val_tar_x, val_obs_mask)
                 dec_id = torch.argmax(g_wta.squeeze(1), dim=-1)
                 vp_means = p_wta[dec_id, torch.arange(batch_size), :, :dy]
-                val_loss4 += mse_loss(vp_means, tr_wta).item()
+                val_loss4 += mse_loss(vp_means, val_tar_y).item()
 
-                p_wta, g_wta = model8(o_wta, t_wta)
+                p_wta, g_wta = model8(val_obs, val_tar_x, val_obs_mask)
                 dec_id = torch.argmax(g_wta.squeeze(1), dim=-1)
                 vp_means = p_wta[dec_id, torch.arange(batch_size), :, :dy]
-                val_loss8 += mse_loss(vp_means, tr_wta).item()
+                val_loss8 += mse_loss(vp_means, val_tar_y).item()
 
             ve2.append(val_loss2)
             ve4.append(val_loss4)
@@ -251,21 +279,26 @@ for epoch in range(epochs):
                 min_vl8 = val_loss8
                 print(f'New best 8: {min_vl8}')
                 torch.save(model8.state_dict(), f'{root_folder}saved_models/wta8.pt')
+            
+            print(f'Bests: {min_vl2}, {min_vl4}, {min_vl8}')
   
         # if epoch % (val_per_epoch*10) == 0:
         #     draw_val_plot(root_folder, epoch)
 
-
-    avg_loss2 += epoch_loss2/epoch_iter
-    avg_loss4 += epoch_loss4/epoch_iter
-    avg_loss8 += epoch_loss8/epoch_iter
+    avg_loss2 += epoch_loss2
+    avg_loss4 += epoch_loss4
+    avg_loss8 += epoch_loss8
 
     if epoch % val_per_epoch == 0:
         print("Epoch: {}, WTA-Losses: {}, {}, {}".format(epoch, avg_loss2/val_per_epoch, avg_loss4/val_per_epoch, avg_loss8/val_per_epoch))
         avg_loss2, avg_loss4, avg_loss8 = 0, 0, 0
 
-# torch.save(torch.Tensor(training_loss_wta), wta_tr_loss_path)
-# torch.save(torch.Tensor(validation_error_wta), wta_val_err_path)
+torch.save(torch.Tensor(tl2), cnep_tl_path+'_2')
+torch.save(torch.Tensor(ve2), cnep_ve_path+'_2')
+torch.save(torch.Tensor(tl4), cnep_tl_path+'_4')
+torch.save(torch.Tensor(ve4), cnep_ve_path+'_4')
+torch.save(torch.Tensor(tl8), cnep_tl_path+'_8')
+torch.save(torch.Tensor(ve8), cnep_ve_path+'_8')
 
 # %%
 
