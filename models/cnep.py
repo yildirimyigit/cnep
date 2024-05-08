eb51d962fff4f63c09a2f30263432739282b7dda
Yigit Yildirim <yigit.yildirim@boun.edu.tr>
2024-05-08 09:32:35 +0300

    naming

--- models/cnep.py
+++ models/cnep.py
@@ -7,14 +7,14 @@ import torch.nn.functional as F
 Batched and masked implementation of the CNEP model
 '''
 class CNEP(nn.Module):
-    def __init__(self, input_dim=1, output_dim=1, n_max_obs=10, n_max_tar=10, encoder_hidden_dims=[256,256,256],
+    def __init__(self, input_dim=1, output_dim=1, n_max=10, m_max=10, encoder_hidden_dims=[256,256,256],
                  num_decoders=4, decoder_hidden_dims=[128,128], batch_size=32, nll_coef=16.81, batch_entropy_coef=10.672, ind_entropy_coef=7.553, scale_coefs=False, device='cuda') -> None:
         super(CNEP, self).__init__()
 
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.n_max = n_max_obs
-        self.m_max = n_max_tar
+        self.n_max = n_max
+        self.m_max = m_max
         self.encoder_num_layers = len(encoder_hidden_dims)
         assert self.encoder_num_layers > 1, "Encoder must have more than 1 hidden layer"
         self.num_decoders = num_decoders
@@ -63,14 +63,14 @@ class CNEP(nn.Module):
         self.to(self.device)
 
     def forward(self, obs, tar, obs_mask, latent=False):
-        # obs: (batch_size, n_max_obs, input_dim+output_dim)
-        # tar: (batch_size, n_max_tar, input_dim)
-        # obs_mask: (batch_size, n_max_obs)
+        # obs: (batch_size, n_max, input_dim+output_dim)
+        # tar: (batch_size, m_max, input_dim)
+        # obs_mask: (batch_size, n_max)
 
         # encoding
         encoded_obs = self.encoder(obs)  # (batch_size,  encoder_hidden_dims[-1])
-        obs_mask_exp = obs_mask.unsqueeze(-1).type_as(encoded_obs)  # (batch_size, n_max_obs, 1)
-        masked_encoded_obs = encoded_obs * obs_mask_exp  # (batch_size, n_max_obs, encoder_hidden_dims[-1])
+        obs_mask_exp = obs_mask.unsqueeze(-1).type_as(encoded_obs)  # (batch_size, n_max, 1)
+        masked_encoded_obs = encoded_obs * obs_mask_exp  # (batch_size, n_max, encoder_hidden_dims[-1])
 
         # masked mean
         sum_masked_encoded_obs = masked_encoded_obs.sum(dim=1)  # (batch_size, encoder_hidden_dims[-1])
@@ -93,14 +93,14 @@ class CNEP(nn.Module):
         return pred, gate_vals
     
     def val(self, obs, tar, obs_mask, latent=False):
-        # obs: (batch_size, n_max_obs, input_dim+output_dim)
+        # obs: (batch_size, n_max, input_dim+output_dim)
         # tar: (batch_size, t_steps, input_dim)
-        # obs_mask: (batch_size, n_max_obs)
+        # obs_mask: (batch_size, m_max)
 
         # encoding
-        encoded_obs = self.encoder(obs)  # (batch_size,  encoder_hidden_dims[-1])
-        obs_mask_exp = obs_mask.unsqueeze(-1).type_as(encoded_obs)  # (batch_size, n_max_obs, 1)
-        masked_encoded_obs = encoded_obs * obs_mask_exp  # (batch_size, n_max_obs, encoder_hidden_dims[-1])
+        encoded_obs = self.encoder(obs)  # (batch_size, n_max, encoder_hidden_dims[-1])
+        obs_mask_exp = obs_mask.unsqueeze(-1).type_as(encoded_obs)  # (batch_size, n_max, 1)
+        masked_encoded_obs = encoded_obs * obs_mask_exp  # (batch_size, n_max, encoder_hidden_dims[-1])
 
         # masked mean
         sum_masked_encoded_obs = masked_encoded_obs.sum(dim=1)  # (batch_size, encoder_hidden_dims[-1])
@@ -124,10 +124,10 @@ class CNEP(nn.Module):
         return pred, gate_vals
 
     def loss(self, pred, gate_vals, real, tar_mask):
-        # pred: (num_decoders, batch_size, n_max_tar, 2*output_dim)
-        # real: (batch_size, n_max_tar, output_dim)
+        # pred: (num_decoders, batch_size, m_max, 2*output_dim)
+        # real: (batch_size, m_max, output_dim)
         # gate_vals: (batch_size, num_decoders)
-        # tar_mask: (batch_size, n_max_tar)
+        # tar_mask: (batch_size, m_max)
 
         pred_mean = pred[:, :, :, :self.output_dim]
         pred_std = F.softplus(pred[:, :, :, self.output_dim:]) + 1e-6  # predicted value is std. In comb. with softplus and minor addition to ensure positivity
